0576e29d880e8674135d44cea9fd390221a6ce88
mraheeldev <m.raheeldev@gmail.com>
2022-07-07 17:58:34 +0500

    under_dev

---
+++ app/src/main/java/com/example/utilitiespkg/utils/ConnectivityAgent.kt
@@ -0,0 +1,13 @@
+package com.example.utilitiespkg.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+
+class ConnectivityAgent(private val context: Context) {
+
+    fun isDeviceConnectedToInternet(): Boolean {
+        val service = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = service.activeNetworkInfo
+        return activeNetwork != null && activeNetwork.isConnected
+    }
+}
